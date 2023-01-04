7ca157b885a7731a99cfb89c71e7d8782e8cb986
Jonas Diemer <jonasdiemer@gmail.com>
2023-01-04 16:36:24 +0100

    Added support for .koala files as input

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
 use std::fs;
 use std::path::PathBuf;
+use std::io::Read;
 use clap::Parser;
+use zip;
+
 use koala2midi::{koala, midi_export};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
    /// Name of the person to greet
-   sequence_file: PathBuf,
+   input_file: PathBuf,
 }
 
 fn main() {
     let args = Args::parse();
     
-    let mut midi_file_path = args.sequence_file.clone();
+    let ext = args.input_file.extension().expect("invalid extension");
+    let mut midi_file_path = args.input_file.clone();
     midi_file_path.set_extension("mid");
+    let mut data = String::from("");
+
+    if ext == "json" {
+
+        let sequence_file_path = args.input_file;
     
-    let data = fs::read_to_string(args.sequence_file)
+        data = fs::read_to_string(sequence_file_path)
         .expect("Unable to read file");
+
+    } else if ext == "koala" {
+
+        let file = fs::File::open(args.input_file).unwrap();
+        let mut archive = zip::ZipArchive::new(file).unwrap();
+
+        let mut sequence_file = archive.by_name("sequence.json").unwrap();
+        sequence_file.read_to_string(&mut data).unwrap();
+    }
 
     let seq_file: koala::SequenceFile = serde_json::from_str(&data)
         .expect("JSON does not have correct format.");
     
-
     midi_export::koala_sequence_to_midi(seq_file, midi_file_path);
 }
